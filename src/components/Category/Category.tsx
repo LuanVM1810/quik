976c8644cca5e0baafd9c028a7e1355695bd6995
LuanVM1810 <Vomongluan@gmail.com>
2024-08-14 17:07:22 +0700

    fix navbar mobile

--- src/components/Category/Category.tsx
+++ src/components/Category/Category.tsx
@@ -1,12 +1,12 @@
 const Category = () => {
   return (
     <div className="flex items-center gap-4 text-sm">
-      <button className="p-[2px] border-solid border-2 bg-gradient-to-b from-[#42FCAC] via-[#36B2F0] to-[#506DF7] rounded-3xl">
+      <button className="p-[2px] w-[70%] bg-gradient-to-b from-[#42FCAC] via-[#36B2F0] to-[#506DF7] rounded-3xl">
         <div className="rounded-3xl p-2 bg-white w-full h-full">
           Không gian làm việc chung
         </div>
       </button>
-      <button className="p-[2px] border-solid border-2 bg-gradient-to-b from-[#42FCAC] via-[#36B2F0] to-[#506DF7] rounded-3xl">
+      <button className="p-[2px] w-[30%] bg-gradient-to-b from-[#42FCAC] via-[#36B2F0] to-[#506DF7] rounded-3xl">
         <div className="rounded-3xl p-2 bg-white w-full h-full">Study Hub</div>
       </button>
     </div>
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -19,22 +19,23 @@ const Header = () => {
         className="ml-auto rounded-full cursor-pointer"
       />
       <div
-        className={`fixed w-[50%] ${
+        className={`fixed w-[85%] md:w-[50%] ${
           openMenu === true ? `translate-x-0` : `translate-x-[100%]`
         }  top-0 right-0 bottom-0 bg-white
-        transition-all duration-500 pl-6 pr-2 py-4 `}
+        transition-all duration-500 pl-8 pr-4 py-4 `}
       >
         <IoCloseOutline
           onClick={clickOpenMenu}
-          size={"24px"}
+          size={"30px"}
           className="ml-auto mb-4 hover:bg-gray-500 rounded-full cursor-pointer"
         />
-        <ul className="font-semibold text-xl flex flex-col gap-4">
+        <ul className="font-bold text-xl flex flex-col gap-4">
           <li className="flex items-center justify-between hover:text-gray-500 cursor-pointer">
             Trang chủ <GoChevronRight size={"24px"} />
           </li>
           <li className="flex items-center justify-between hover:text-gray-500 cursor-pointer">
-            Lịch sử <GoChevronRight size={"24px"} />
+            Lịch sử giao dịch
+            <GoChevronRight size={"24px"} />
           </li>
           <li className="flex items-center justify-between hover:text-gray-500 cursor-pointer">
             Về chúng tôi <GoChevronRight size={"24px"} />
@@ -43,6 +44,24 @@ const Header = () => {
             Hồ sơ <GoChevronRight size={"24px"} />
           </li>
         </ul>
+        <div className="my-20 cursor-pointer font-semibold">Quik</div>
+        <div className="text-gray-500 text-xl">
+          <p>
+            Become a Quik Member for best products, inspiration and stories in
+            work.{" "}
+            <span className="font-bold text-[#2C2C2C] cursor-pointer">
+              Learn more
+            </span>
+          </p>
+          <div className="flex my-3 text-base font-bold gap-2 mt-[30px]">
+            <button className="bg-[#2c2c2c] hover:bg-gray-500 text-white px-4 py-2 rounded-3xl">
+              Tham gia
+            </button>
+            <button className="bg-white hover:bg-gray-500 border-solid border-[1px] px-4 py-2 rounded-3xl border-gray-600 text-[#2c2d2c]">
+              Đăng nhập
+            </button>
+          </div>
+        </div>
       </div>
     </nav>
   );
